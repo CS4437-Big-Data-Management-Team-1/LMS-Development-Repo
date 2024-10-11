b5253698a5ba333311a1364da0230ed2a9126497
Olan Healy <olanhealy12@gmail.com>
2024-10-11 21:04:57 +0100

    Setup of user-service microservice

---
+++ user-service/src/test/java/com/lms/userservice/UserServiceTests.java
@@ -0,0 +1,12 @@
+package com.lms.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
