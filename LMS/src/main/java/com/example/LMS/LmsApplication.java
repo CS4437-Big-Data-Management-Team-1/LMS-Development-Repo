ebc34a00b868c9750e4f03253082dc3aa53beef0
Caoimhin <116285183+CaoimhinHawk1@users.noreply.github.com>
2024-10-21 16:09:01 +0100

    Added a hello message to the webapp for testing

--- LMS/src/main/java/com/example/LMS/LmsApplication.java
+++ LMS/src/main/java/com/example/LMS/LmsApplication.java
@@ -1,13 +1,19 @@
 package com.example.LMS;
 
 import org.springframework.boot.SpringApplication;
+import org.springfrmework.web.bind.annotation.GetMapping
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LmsApplication {
 
+	@GetMapping("/")
+	public String home(){
+		return "Hello from the other side :)";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
 	}
+
 
 }
