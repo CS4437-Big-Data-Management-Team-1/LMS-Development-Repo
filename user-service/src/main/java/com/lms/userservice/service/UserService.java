47fa69417daf912c4b318fb5a948b4ffa80d41cd
Mark <21335265@studentmail.ul.ie>
2024-10-21 18:56:29 +0100

    database functionality added to user service

--- user-service/src/main/java/com/lms/userservice/service/UserService.java
+++ user-service/src/main/java/com/lms/userservice/service/UserService.java
@@ -1,4 +1,13 @@
 package com.lms.userservice.service;
 
+import com.lms.userservice.database.UserDatabaseConnector;
+
 public class UserService {
+    public static void main(String[] args) {
+        UserDatabaseConnector database = new UserDatabaseConnector();
+        database.connectToDB();
+        database.addUserToDB();
+        database.disconnectFromDB();
+
+    }
 }
