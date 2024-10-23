cfd4c1773c08931184e1e2a45580793ee7bdf01b
Cathal <cathalcrowepatrick@gmail.com>
2024-10-23 14:09:42 +0100

    Basic Notification Foundation

--- notification-service/src/main/java/com/lms/notificationservice/retry/NotificationRetryHandler.java
+++ notification-service/src/main/java/com/lms/notificationservice/retry/NotificationRetryHandler.java
@@ -1,5 +1,10 @@
 package com.lms.notificationservice.retry;
 
+
+import com.lms.notificationservice.model.Notification;
+
 public class NotificationRetryHandler {
-    
+    public void handleRetry(Notification notification) {
+        System.out.println("Retrying to send notification: " + notification.getMessage());
+    }
 }
