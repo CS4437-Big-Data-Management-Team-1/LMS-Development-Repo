d737b55be8dd86a94a59c690d4364e1e358f6f1d
Cathal <cathalcrowepatrick@gmail.com>
2024-10-24 17:54:56 +0100

    Simple implementation WIP

--- notification-service/src/main/java/com/lms/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/lms/notificationservice/NotificationServiceApplication.java
@@ -1,13 +1,21 @@
 package com.lms.notificationservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
+	@Autowired
+	private EmailSenderService emailSenderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
-
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() {
+		emailSenderService.sendEmail("lastmanstanding.notifier@gmail.com", "Test Subject", "Test Body");
+	}
 }
