f72044f1eb665bbeb54183965e25e075b4d5d5ff
Cathal <cathalcrowepatrick@gmail.com>
2024-10-24 19:55:16 +0100

    Implemented functioning email service. Run NotificationServiceApplication.java to send an email from lastmanstanding.notifier@gmail.com to itself / change recipient email. Removed redundant code from other classes for now.

--- notification-service/src/main/java/com/lms/notificationservice/repository/NotificationRepository.java
+++ notification-service/src/main/java/com/lms/notificationservice/repository/NotificationRepository.java
@@ -1,19 +1,3 @@
 package com.lms.notificationservice.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.lms.notificationservice.model.Notification;
-
-public class NotificationRepository {
-    private List<Notification> notificationLog = new ArrayList<>();
-
-    public void save(Notification notification) {
-        notificationLog.add(notification);
-        System.out.println("Notification logged: " + notification.getMessage());
-    }
-
-    public List<Notification> findAll() {
-        return notificationLog;
-    }
-}
+public class NotificationRepository {}
