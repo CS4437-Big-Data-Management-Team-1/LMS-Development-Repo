7d14b01d2fb35f03d5d3a2ec2404ce8957349a9a
Cathal <cathalcrowepatrick@gmail.com>
2024-10-25 19:45:03 +0100

    Env file used for app password. Commented out test to allow maven build

--- notification-service/src/main/java/com/lms/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/lms/notificationservice/NotificationServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import com.lms.notificationservice.model.Notification;
 import com.lms.notificationservice.service.NotificationService;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
@@ -15,6 +16,8 @@ public class NotificationServiceApplication {
 	private Notification notification = new Notification("lastmanstanding.notifier@gmail.com", "Hello", "Test Subject");
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("NOTIFICATION_SERVICE_APP_PASSWORD", dotenv.get("NOTIFICATION_SERVICE_APP_PASSWORD"));
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 	@EventListener(ApplicationReadyEvent.class)
