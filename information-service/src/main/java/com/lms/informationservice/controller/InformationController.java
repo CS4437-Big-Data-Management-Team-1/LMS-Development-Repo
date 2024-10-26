ecb69b51ccaaf0bfd3534b9fcb967b897e2fe812
caoimhe <caoimh3cahill@gmail.com>
2024-10-26 15:05:15 +0100

    information class outlines

---
+++ information-service/src/main/java/com/lms/informationservice/controller/InformationController.java
@@ -0,0 +1,5 @@
+package com.lms.informationservice.controller;
+
+public class InformationController {
+
+}
---
+++ information-service/src/main/java/com/lms/informationservice/service/InformationService.java
@@ -0,0 +1,4 @@
+package com.lms.informationservice.service;
+
+public class InformationService {
+}
