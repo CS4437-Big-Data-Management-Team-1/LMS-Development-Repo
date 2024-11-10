b1d50434fbfb8a28e300e8d3bcaadfde9fdf07c0
Mark <21335265@studentmail.ul.ie>
2024-11-10 14:32:33 +0000

    fixed some very small issues

---
+++ payment-service/src/main/java/com/lms/paymentservice/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.lms.paymentservice.controller;
+
+import com.lms.paymentservice.model.PaymentRequest;
+import com.lms.paymentservice.service.PaymentService;
+import com.lms.paymentservice.model.PaymentResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/process")
+    public PaymentResponse processPayment(@RequestBody PaymentRequest paymentRequest) {
+        return paymentService.processPayment(paymentRequest);
+    }
+}
