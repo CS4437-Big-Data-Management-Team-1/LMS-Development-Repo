930f5fff9979685a2d9d10c933bedcc4f4a97ad3
Olan Healy <olanhealy12@gmail.com>
2024-11-11 19:29:09 +0000

    Some auth stuff for game service added in

---
+++ game-service/src/main/java/com/lms/gameservice/service/AuthService.java
@@ -0,0 +1,32 @@
+package com.lms.gameservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String validateToken(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorisation", token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "http://localhost:8080/api/users/validate-token",
+                HttpMethod.POST,
+                entity,
+                String.class
+        );
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody();
+        } else {
+            throw new RuntimeException("Invalid or expired token");
+        }
+    }
+}
