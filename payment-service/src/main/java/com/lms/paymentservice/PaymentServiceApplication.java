16422a0db3d4243908ab58e858eb73d5d4673d51
Callum <callumcarroll70@gmail.com>
2024-11-12 11:53:40 +0000

    resolved merge conflicts caused by docs

--- payment-service/src/main/java/com/lms/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/lms/paymentservice/PaymentServiceApplication.java
@@ -4,8 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.github.cdimascio.dotenv.Dotenv;
 
+/**
+ * Main entry point for the Payment Service Application.
+ *
+ * This class initializes the application, loads environment variables,
+ * and configures system properties necessary for the application to function.
+ *
+ * @author Callum Carroll
+ */
+
 @SpringBootApplication
 public class PaymentServiceApplication {
+
+    /**
+     * This method loads environment variables using the Dotenv library, setting the
+     * Stripe API key as a system property, along with the DB connectors.
+     * It then starts the Spring application context.
+     */
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
