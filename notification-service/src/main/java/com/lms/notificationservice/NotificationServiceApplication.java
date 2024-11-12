c9454e06fedac7ec77d6a1f46efab818d4098170
Cathal <cathalcrowepatrick@gmail.com>
2024-11-12 16:01:53 +0000

    Removal of unneeded files + variables

--- notification-service/src/main/java/com/lms/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/lms/notificationservice/NotificationServiceApplication.java
@@ -1,26 +1,14 @@
 package com.lms.notificationservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
 
-import com.lms.notificationservice.controller.NotificationController;
-
 import io.github.cdimascio.dotenv.Dotenv;
-import com.lms.notificationservice.database.NotificationDatabaseController;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Autowired
-    private NotificationController notificationController;
-
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
         System.setProperty("NOTIFICATION_SERVICE_APP_PASSWORD", dotenv.get("NOTIFICATION_SERVICE_APP_PASSWORD"));
