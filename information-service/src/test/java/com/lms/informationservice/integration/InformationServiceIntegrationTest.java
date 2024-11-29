827e06014a769a14301e5a0ef9876b9e0937789d
caoimhe <caoimh3cahill@gmail.com>
2024-11-29 19:25:13 +0000

    small fixes

--- information-service/src/test/java/com/lms/informationservice/integration/InformationServiceIntegrationTest.java
+++ information-service/src/test/java/com/lms/informationservice/integration/InformationServiceIntegrationTest.java
@@ -5,7 +5,7 @@ import com.lms.informationservice.repository.TeamRepository;
 import com.lms.informationservice.service.InformationService;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
