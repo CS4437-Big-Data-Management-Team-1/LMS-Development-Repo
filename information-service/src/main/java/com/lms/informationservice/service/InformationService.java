51260a4158eeb4c173915fd1f6e91ffe34f805be
Caoimhin <21307571@studentmail.ul.ie>
2024-12-03 18:32:48 +0000

    getting rid of dotenv load in InformationService

--- information-service/src/main/java/com/lms/informationservice/service/InformationService.java
+++ information-service/src/main/java/com/lms/informationservice/service/InformationService.java
@@ -48,7 +48,6 @@ public class InformationService {
     public InformationService(TeamRepository teamRepository, MatchesRepository matchesRepository) {
         this.teamRepository = teamRepository;
         this.matchesRepository = matchesRepository;
-        Dotenv dotenv = Dotenv.load();
         this.webClient = WebClient.builder()
                 .baseUrl("FOOTBALL_API_BASE_URL")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
